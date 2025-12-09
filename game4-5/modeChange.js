fe92959cdfd96490903d5984e809cb14e3b7ea2e
Ruwani Sugandika <sugandikarh@gmail.com>
2025-12-09 23:41:28 +0530

    add button to change easy/hard version

---
+++ game4-5/modeChange.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+function leftClick(){
+    btn.style.left = "0";
+    window.location.href = "countingGame_Easy.php";
+}
+function rightClick(){
+    btn.style.left = "110px";
+    // window.location.href = "countingGame_Hard.php";
+}
